73ce3b8a422a7cd65ae5e526b629eb4812da6407
Abdullah Yılmaz <ay1610405@gmail.com>
2026-05-09 19:36:29 +0300

    Otomatik server yeniden başlatma için nodemon eklendi

--- data/users.js
+++ data/users.js
@@ -5,6 +5,16 @@ const users = [
         id: 1,
         username: "test",
         password: bcrypt.hashSync("123456", 10)
+    },
+    {
+        id: 2,
+        username: "admin",
+        password: bcrypt.hashSync("123", 10)
+    },
+    {
+        id: 3,
+        username: "apo",
+        password: bcrypt.hashSync("apo123", 10)
     }
 ];
 
